e44dfa1411a1e5b03e1751e63aaa274bac7e5489
nikhilg07 <43913706+nikhilg07@users.noreply.github.com>
2022-03-25 12:37:35 +0530

    Delete search_microservice directory

--- search_microservice/src/main/java/com/quinbay/ecommerce/search_microservice/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.quinbay.ecommerce.search_microservice.service;
-
-
-import com.quinbay.ecommerce.search_microservice.entity.ProductEntity;
-
-public interface ProductService {
-
-    void addProduct(ProductEntity productEntity);
-}
